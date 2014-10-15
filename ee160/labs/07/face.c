8bef8e9ca8db68b14186091bbf3eafd1ad30199f
Zhao Li <zhaol@hawaii.edu>
2014-10-15 06:21:52 +0000

    adding lab 7 displayer sample code

---
+++ ee160/labs/07/face.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void face();
+
+int main () {
+    
+    face();
+    
+    return 0;
+}
+
+void face() {
+    printf("--------\n");
+    
+}
---
+++ ee160/labs/07/print_strings.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#define eye_balls "| . . |\n"
+
+int main() {
+    printf("%s", eye_balls);    
+    
+    return 0;
+}
