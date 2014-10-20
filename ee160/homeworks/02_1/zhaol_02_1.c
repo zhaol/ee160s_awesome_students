e4552a06823245dbb72eab6999176b32d993ff34
Zhao Li <zhaol@hawaii.edu>
2014-10-20 10:28:58 +0000

    demo self checking program

---
+++ ee160/homeworks/02_1/zhaol_02_1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main () {
+    printf("It was nice meeting you, world.\n");
+    printf("Hope to see you again soon. :-\n");
+    printf("Don't forget to write.\n");
+    return 0;
+}
